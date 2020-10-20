88193efb3f7e9db3034bf8336ed18c451dd0d3e7
kiervz <kierveymaghanoy24@gmail.com>
2020-10-20 11:34:15 +0800

    Install vue-router

---
+++ resources/js/Components/ExampleComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-body">
+                        I'm an Example component. {{message}}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ExampleComponents",
+        data() {
+            return {
+                message: "Hello"
+            }
+        },
+        mounted() {
+            console.log('Example Component mounted.')
+        }
+    }
+</script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,21 @@
 require('./bootstrap');
+
+window.Vue = require('vue');
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter);
+
+const routes = [
+    { path: '/hello', component: require('./components/Hello.vue').default },
+    { path: '/example', component: require('./components/ExampleComponent.vue').default },
+]
+
+const router = new VueRouter({
+    mode: 'history',
+    routes // short for `routes: routes`
+});
+
+const app = new Vue({
+  el: '#app',
+  router
+});
